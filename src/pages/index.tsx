8ae307aa30228c89addc33ca49fc9848f3c43bc2
taka2 <taka23131@gmail.com>
2022-06-14 22:44:16 +0900

    投稿を見るためのコンポーネントの雛形を作った

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import type { NextPage } from 'next'
-import { VStack, Box } from '@chakra-ui/react'
+import { VStack, Box, Stack } from '@chakra-ui/react'
 import Header from '../components/header'
+import PostPreview from '../components/PostPreview'
 
 const Home: NextPage = () => {
   return (
     <VStack>
       <Header></Header>
-      <Box>ボディーだよ</Box>
+      <Stack>
+        <PostPreview title='hoge'></PostPreview>
+        <PostPreview title='age'></PostPreview>
+        <PostPreview title='uge'></PostPreview>
+      </Stack>
     </VStack>
   )
 }
