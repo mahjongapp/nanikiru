13ab745d443f645c430944eeda530635011ea0ba
taka2 <taka23131@gmail.com>
2022-06-14 22:07:26 +0900

    ヘッダーの雛形を作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import type { NextPage } from 'next'
-import { Box } from '@chakra-ui/react'
+import { VStack, Box } from '@chakra-ui/react'
+import Header from '../components/header'
 
 const Home: NextPage = () => {
-  return <Box>Hello World!</Box>
+  return (
+    <VStack>
+      <Header></Header>
+      <Box>ボディーだよ</Box>
+    </VStack>
+  )
 }
 
 export default Home
