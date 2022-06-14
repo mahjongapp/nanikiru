c417e3e6dc90b018fd86f95a80266a16e792ed9f
taka2 <taka23131@gmail.com>
2022-06-14 23:47:02 +0900

    PostPreviewが中央揃えになるようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next'
-import { Stack, Box } from '@chakra-ui/react'
+import { Stack, VStack } from '@chakra-ui/react'
 import Header from '../components/header'
 import PostPreview from '../components/PostPreview'
 
@@ -7,11 +7,11 @@ const Home: NextPage = () => {
   return (
     <Stack>
       <Header></Header>
-      <Stack>
+      <VStack>
         <PostPreview title='hoge'></PostPreview>
         <PostPreview title='age'></PostPreview>
         <PostPreview title='uge'></PostPreview>
-      </Stack>
+      </VStack>
     </Stack>
   )
 }
