6e90f0df3badcb733fb20a2c1b95ba0f14b300b4
taka2 <taka23131@gmail.com>
2022-06-16 10:04:28 +0900

    posteditのformを作成

--- src/pages/postedit/index.tsx
+++ src/pages/postedit/index.tsx
@@ -1,12 +1,17 @@
-import { Box, Stack } from '@chakra-ui/react'
+import { Button, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react'
 import Header from '../../components/header'
 
 export default function PostEdit() {
   return (
-    // eslint-disable-next-line react/jsx-no-undef
     <Stack>
       <Header isPostEdit></Header>
-      <Box>投稿ページだよ</Box>
+      <FormControl>
+        <FormLabel>タイトル</FormLabel>
+        <Input></Input>
+        <FormLabel>本文</FormLabel>
+        <Input></Input>
+        <Button>送信</Button>
+      </FormControl>
     </Stack>
   )
 }
