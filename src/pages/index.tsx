a687b5cf56d7010c93adebed4f095c5d42be86bb
taka2 <taka23131@gmail.com>
2022-06-19 20:30:28 +0900

    投稿一覧が見られるようにした

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,24 @@ import type { NextPage } from 'next'
 import { Stack, VStack } from '@chakra-ui/react'
 import Header from '../components/header'
 import PostPreview from '../components/PostPreview'
+import client from '../lib/client'
+import { useQuery } from 'react-query'
 
 const Home: NextPage = () => {
+  const { data, isLoading, isError } = useQuery('getPosts', () => client.GetPosts())
+  console.log(data)
   return (
     <Stack spacing={8}>
       <Header></Header>
       <VStack spacing={8}>
-        <PostPreview title='hoge'></PostPreview>
-        <PostPreview title='age'></PostPreview>
-        <PostPreview title='uge'></PostPreview>
+        {data?.posts.map((post, index) => (
+          <PostPreview
+            key={index}
+            title={post?.title}
+            body={post?.body}
+            choices={post?.choices}
+          ></PostPreview>
+        ))}
       </VStack>
     </Stack>
   )
