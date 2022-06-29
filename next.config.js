982456126d1ce1105a4e9b656fdb6d51f433f57e
taka2 <taka23131@gmail.com>
2022-06-30 00:42:14 +0900

    NextConfigの変更

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
     images: {
         domains: ["firebasestorage.googleapis.com"]
     }
 }
+
+module.exports = nextConfig
