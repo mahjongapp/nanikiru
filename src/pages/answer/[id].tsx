08423f1a4ed13d562597b8d0335f686dedb753a4
taka2 <taka23131@gmail.com>
2022-06-29 23:19:34 +0900

    /answer/:idにてssrを導入

--- src/pages/answer/[id].tsx
+++ src/pages/answer/[id].tsx
@@ -18,13 +18,15 @@ import Header from '../../components/header'
 import PostPreview from '../../components/PostPreview'
 import { Answer } from '../../components/Answer'
 import { Controller, useForm } from 'react-hook-form'
+import { prisma } from '../../lib/prisma'
+import { Post } from '@prisma/client'
 
 type Input = {
   body: string
   choice: string
 }
 
-export default function AnswerPage() {
+export default function AnswerPage({ post }: Props) {
   const {
     register,
     handleSubmit,
@@ -32,10 +34,10 @@ export default function AnswerPage() {
     formState: { errors },
     reset,
   } = useForm<Input>()
+  console.log(post)
   const router = useRouter()
   const { id } = router.query
-  const { data } = useQuery('GetPostByID', () => client.GetPostByID({ postId: Number(id) }))
-  const { data: answers } = useQuery('GetAnswersByPostId', () =>
+  const { data: answers, isLoading: isAnswerLoading } = useQuery('GetAnswersByPostId', () =>
     client.GetAnswersByPostId({ postId: Number(id) }),
   )
   const { mutate, isLoading } = useMutation((data: Input) =>
@@ -54,61 +56,99 @@ export default function AnswerPage() {
     <Stack>
       {isLoading && <Progress size='xs' isIndeterminate />}
       <Header />
-      {data === undefined ? (
-        <Box>Loading</Box>
-      ) : (
-        <VStack>
-          <PostPreview
-            id={data?.post.id}
-            title={data?.post.title}
-            body={data?.post.body}
-            imgurl={data?.post.imgurl}
-            choices={data?.post.choices}
-          />
-          <Box>
-            <Stack as='form' onSubmit={handleSubmit(onSubmit)}>
-              <FormControl>
-                <FormLabel>選択肢</FormLabel>
-                <Controller
-                  control={control}
-                  name='choice'
-                  render={({ field }) => (
-                    <RadioGroup {...field}>
-                      <Stack direction='row'>
-                        {data?.post.choices?.map((choice, index) => (
-                          <Radio key={index} value={choice?.id.toString()}>
-                            {choice?.name}
-                          </Radio>
-                        ))}
-                      </Stack>
-                    </RadioGroup>
-                  )}
-                  rules={{
-                    required: { value: true, message: 'この項目は必要です' },
-                  }}
-                />
-                <Box>{errors.choice && errors.choice.message}</Box>
-                <FormLabel>コメント</FormLabel>
-                <Textarea {...register('body', { required: true })} />
-                <Box>{errors.body && 'コメントを入力して下さい'}</Box>
-              </FormControl>
-              <Button
-                disabled={isLoading}
-                type='submit'
-                size='lg'
-                mx={8}
-                my={2}
-                colorScheme={'green'}
-              >
-                送信
-              </Button>
-            </Stack>
-          </Box>
-          {answers?.answersByPostId.map((answer, index) => (
+
+      <VStack>
+        <PostPreview
+          id={post.id}
+          title={post.title}
+          body={post.body}
+          imgurl={post.imgurl}
+          choices={post.choices}
+        />
+        <Box>
+          <Stack as='form' onSubmit={handleSubmit(onSubmit)}>
+            <FormControl>
+              <FormLabel>選択肢</FormLabel>
+              <Controller
+                control={control}
+                name='choice'
+                render={({ field }) => (
+                  <RadioGroup {...field}>
+                    <Stack direction='row'>
+                      {post.choices.map((choice, index) => (
+                        <Radio key={index} value={choice?.id.toString()}>
+                          {choice?.name}
+                        </Radio>
+                      ))}
+                    </Stack>
+                  </RadioGroup>
+                )}
+                rules={{
+                  required: { value: true, message: 'この項目は必要です' },
+                }}
+              />
+              <Box>{errors.choice && errors.choice.message}</Box>
+              <FormLabel>コメント</FormLabel>
+              <Textarea {...register('body', { required: true })} />
+              <Box>{errors.body && 'コメントを入力して下さい'}</Box>
+            </FormControl>
+            <Button
+              disabled={isLoading}
+              type='submit'
+              size='lg'
+              mx={8}
+              my={2}
+              colorScheme={'green'}
+            >
+              送信
+            </Button>
+          </Stack>
+        </Box>
+        {isAnswerLoading ? (
+          <Box>loading...</Box>
+        ) : (
+          answers?.answersByPostId.map((answer, index) => (
             <Answer key={index} choice={answer?.choice?.name} body={answer?.body} />
-          ))}
-        </VStack>
-      )}
+          ))
+        )}
+      </VStack>
     </Stack>
   )
+}
+
+type Props = {
+  post: {
+    id?: number
+    imgurl?: string
+    title?: string
+    body?: string
+    choices: {
+      id: number
+      name: string
+    }[]
+  }
+}
+
+export async function getServerSideProps(context: { params: { id: string } }) {
+  const post = await prisma?.post.findUnique({
+    where: {
+      id: Number(context.params.id),
+    },
+    include: {
+      choices: true,
+    },
+  })
+  return {
+    props: {
+      post: {
+        id: post?.id,
+        imgurl: post?.imgurl,
+        title: post?.title,
+        body: post?.body,
+        choices: post?.choices.map((choice) => {
+          return { id: choice.id, name: choice.name }
+        }),
+      },
+    },
+  }
 }
